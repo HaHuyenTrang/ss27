b87c680d4c9a86574e0471d8ed16d82542112410
HaHuyenTrang <147359400+HaHuyenTrang@users.noreply.github.com>
2024-05-09 17:09:12 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Bt1 from "./assets/components/Bt1"
+import Bt2 from "./assets/components/Bt2"
+
+import Bt3 from "./assets/components/Bt3"
+import Bt4 from "./assets/components/Bt4"
+import Bt5 from "./assets/components/Bt5"
+import Bt6 from "./assets/components/Bt6"
+import Bt7 from "./assets/components/Bt7"
+
+
+
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <Bt1></Bt1>
+      <Bt2></Bt2>
+      <Bt3></Bt3>
+      <Bt4></Bt4>
+      <Bt5></Bt5>
+      <Bt6></Bt6>
+      <Bt7></Bt7>
+    </div>
+  )
+}
